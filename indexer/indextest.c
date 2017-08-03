3844906383d59c248c41f94e1c3b94b23c18fc95
Christina Lu <c.20@dartmouth.edu>
2017-08-03 08:02:42 -0400

    Added indextest module.

---
+++ indexer/indextest.c
@@ -0,0 +1,38 @@
+/* 
+ * indextest.c - CS50 Lab 5, part of TSE
+ *
+ * The *index tester* is the companion of the indexer; it loads an index file 
+ * produced by the indexer and saves it to another file.
+ *
+ * Christina Lu, August 2017
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "../common/index.h"
+
+/**************** global functions ****************/
+
+/**************** main() ********************/
+
+int main(int argc, char* argv[])
+{
+	// check number of arguments
+	if (argc != 3) {
+		fprintf(stderr, "usage: page_directory filename\n");
+		exit(2);
+	}
+
+	char* old_file = argv[1];
+	char* new_file = argv[2];
+	index_t *ht;
+
+	index_load(ht, old_file);
+	index_save(ht, new_file);
+
+	// clean up
+	index_delete(ht);
+	exit(0);
+}
+
+
