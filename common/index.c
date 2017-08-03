7bbc16e79c5f63fa28faacf109ed3c11e123997f
Christina Lu <c.20@dartmouth.edu>
2017-08-02 20:49:27 -0400

    Updated index module.

--- common/index.c
+++ common/index.c
@@ -14,6 +14,7 @@
 #include "../libcs50/counters.h"
 #include "../libcs50/memory.h"
 #include "../libcs50/file.h"
+#include "word.h"
 #include "index.h"
 
 /**************** file-local global variables ****************/
@@ -44,15 +45,16 @@ index_t *index_new(const int num_slots)
 }
 
 /**************** index_insert() ****************/
-bool index_insert(index_t *ht, const char *key, counters_t *ct)
+bool index_insert(index_t *ht, const char *key, counters_t *ctrs)
 {
-	return hashtable_insert(ht, key, ct);
+	NormalizeWord(key);
+	return hashtable_insert(ht, key, ctrs);
 }
 
 /**************** index_find() ****************/
 counters_t *index_find(index_t *ht, const char *key)
 {
-	return hashtable_find(ht, key);
+	return (counters_t*) hashtable_find(ht, key);
 }
 
 /**************** index_delete() ****************/
@@ -62,22 +64,10 @@ void index_delete(index_t *ht)
 }
  
 /*************** index_save() *****************/
-bool index_save(index_t *ht, char* page_directory, char* filename)
+bool index_save(index_t *ht, char* filename)
 {
-	// create file name
-	int length = strlen(page_directory) + strlen(filename) + 2
-	char* str = count_malloc(sizeof(char) * length);
-	if (str == NULL) {
-		fprintf(stderr, "error allocating for index_save\n");
-		return false;
-	}
-
-	strcpy(str, page_directory);
-	strcat(str, "/");
-	strcat(str, num);
-
 	// create new file
-	FILE *fp = fopen(str, "w");
+	FILE *fp = fopen(filename, "w");
 	if (fp == NULL) {
 		fprintf(stderr, "error opening file %s\n", filename);
 		return false;
@@ -91,7 +81,6 @@ bool index_save(index_t *ht, char* page_directory, char* filename)
 		return false;
 	}
 	
-	count_free(str);	
 	return true;
 }
 
@@ -118,7 +107,7 @@ static void print_count(void *arg, const int key, int count)
 }
 
 /************** index_load() ***************/
-index_t index_load(char* filename)
+index_t *index_load(char* filename)
 {
 	FILE *fp;
 	fp = fopen(filename, "r");
@@ -128,43 +117,24 @@ index_t index_load(char* filename)
 		fprintf(stderr, "index_load allocation failed");
 		return NULL;
 	}
-	
-	char *line;	
+		
 	char *word;
-	int *id;
-	int *count;
-	while((line = readwordp(fp)) != NULL) {
-		// find word
-		if (sscanf(line, "%s", word) == 1) {
-			// make new counter for that word
-			counters_t *ctrs = counters_new();
-			
-			if (ctrs == NULL) {
-				fprintf(stderr, "index_load allocation failed")
-				return NULL;
-			}
-			
-			// put id/count into counter
-			while (sscanf(line, "%d", id) == 1) {
-				if (sscanf(line, "%d", count) == 1) {
-					counters_set(ctrs, id, count);
-				}
-			}
-			
-			index_insert(ht, word, ctrs);
-		} 	
+	int id;
+	int count;
+	while((word = readwordp(fp)) != NULL) {
+		counters_t *ctrs = counters_new();
+		while (fscanf(fp, "%d %d", &id, &count) == 2) {
+			counters_set(ctrs, id, count);
+		}
+		index_insert(ht, word, ctrs);
 	}
 
 	if (fclose(fp) != 0) {
 		fprintf(stderr, "error closing file %s\n", filename);
 		return NULL;
 	}
-		
 	
-	count_free(line;
 	count_free(word);
-	count_free(id);
-	count_free(count);
 	// free others?
 	
 	return ht;
--- common/index.h
+++ common/index.h
@@ -20,7 +20,7 @@ typedef hashtable_t index_t; // opaque to users of the module
 /**************** functions ****************/
 
 /* Create a new (empty) index; return NULL if error. */
-hashtable_t *index_new(const int num_slots);
+index_t *index_new(const int num_slots);
 
 /* Insert counters, idnetified by key (string), into the given index.
  * The key string is copied for use by the index; that is, the module
@@ -30,15 +30,27 @@ hashtable_t *index_new(const int num_slots);
  * Re turn false if key exists in index, any parameter is NULL, or error;
  * return true iff new counters was inserted.
  */
-bool index_insert(hashtable_t *ht, const char *key, counters_t *ct);
+bool index_insert(index_t *ht, const char *key, counters_t *ct);
 
 /* Return the item associated with the given key;
  * return NULL if hashtable is NULL, key is NULL, key is not found.
  */
-void *index_find(hashtable_t *ht, const char *key);
+counters_t *index_find(index_t *ht, const char *key);
 
 /* Delete the whole index; ignore NULL index.
  */
-void index_delete(hashtable_t *ht);
+void index_delete(index_t *ht);
+
+/* Writes the data structure in the memory to an external file. Takes index
+ * to be written to the file, and path name of file to write to. Writes in the
+ * format "word doc_id count [doc_id count]..." one line per word.
+ * Returns true upon sucess, false if error.
+ */
+bool index_save(index_t *ht, char* filename);
+
+/* Loads information from a file and builds an index data structure in the
+ * memory. Returns the index structure.
+ */
+index_t *index_load(char* filename);
 
 #endif // __INDEX_H
