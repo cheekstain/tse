a4abdd1c1c8a2c5c9bc2fc76dd448c245f8ace68
Christina Lu <c.20@dartmouth.edu>
2017-08-12 07:01:24 -0400

    Updated querier with and sequence score counting

--- querier/querier.c
+++ querier/querier.c
@@ -22,7 +22,10 @@
 /* none */
 
 /**************** local types ****************/
-/* none */
+typedef struct two_counters {
+    counters_t *result;
+    counters_t *ctrs;
+} two_counters_t;
 
 /**************** functions ****************/
 bool check_parameters(int argc, char* argv[]);
@@ -30,8 +33,12 @@ void process_query(char* page_directory, index_t* ht);
 int fetch_words(char* line, char** words);
 void print_query(char** words, int count);
 bool check_query(char** words, int count);
-//int num_elements(char** words);
 bool is_literal(char* word);
+void run_query(index_t *ht, char** words, int count);
+index_t* get_scores(index_t *ht, char** words, int count);
+
+static void counters_intersect(counters_t *result, counters_t *ctrs);
+static void counter_intersect_helper(void *arg, int key, int count);
 
 /**************** main() ****************/
 
@@ -59,7 +66,7 @@ int main(int argc, char* argv[])
 bool check_parameters(int argc, char* argv[])
 {
 	if (argc != 3) {
-		fprintf(stderr, "usage: page_directory index_filename");
+		fprintf(stderr, "usage: page_directory index_filename\n");
 		return false;
 	}
 
@@ -92,10 +99,11 @@ void process_query(char* page_directory, index_t* ht)
 	while ((line = readlinep(stdin)) != NULL) {
 		char** words = count_malloc(sizeof(char*) * 20);
 		int count = fetch_words(line, words);
+		words = realloc(words, sizeof(char*) * count);
 		if (count != 0 && words != NULL){
 			print_query(words, count);
 			if (check_query(words, count)) {
-				//run_query(words)
+				run_query(ht, words, count);
 			}
 		}
 	
@@ -103,6 +111,7 @@ void process_query(char* page_directory, index_t* ht)
 		count_free(line);
 		printf("KEY WORDS:> ");
 	}
+	printf("\n");
 }
 
 /* fetch_words() takes the line to find words from and an intialized array to
@@ -196,18 +205,6 @@ bool check_query(char** words, int count)
 	return true;
 }
 
-/* num_elements() takes an array of strings and returns the number of elements
- * in the array.
- */
-int num_elements(char** words)
-{
-	int num = 0;
-	while (words[num] != NULL) {
-		num++;
-	}
-	return num;
-}
-
 /* is_literal() takes a string and returns true if it is a literal and false
  * otherwise. literals: and, or
  */
@@ -218,6 +215,82 @@ bool is_literal(char* word)
 		return true;
 	} 
 	return false;
+}
+
+/* run_query() takes the index, words, and count, and actually runs the
+ * query.
+ */
+void run_query(index_t *ht, char** words, int count)
+{
+	//index_t* scores = get_scores(ht, words, count);
+}
+
+/* get_scores() returns an index which contains the aggregated scores
+ * from all and sequences. it takes the index, the array of words, and count.
+ */
+index_t* get_scores(index_t *ht, char** words, int count)
+{
+	char* word;
+	index_t* all_scores = index_new(200);
+	assertp(all_scores, "Failed to intialize index\n");
+
+	for (int i = 0; i < count; i++) {
+		word = words[i];
+		
+		// if the word is not and/or
+		if (!is_literal(word)) {
+			counters_t *scores = index_find(ht, word);
+			int j = i + 1;
+			char* next = words[j];
+			while ((j < count) && (strcmp(next, "or") != 0)) {
+				if (!is_literal(next)) { // must be and
+															counters_t *ctrs = index_find(ht, next);
+					counters_intersect(scores, ctrs);
+					
+				}
+				j++;
+				if (j < count) {
+					next = words[j];
+				}
+			}
+			i = j;
+			char str[12];
+			char* key = str;
+			sprintf(str, "%d", i);
+			index_insert(all_scores, key, scores);
+					}
+	}
+
+	return all_scores;
+}
+
+/* counter_intersect() takes two counters and changes the first one to
+ * intersection of the two, where the ID is the same but the key
+ * is set to the minimum value.
+ */
+static void counters_intersect(counters_t *result, counters_t *ctrs)
+{
+        assertp(result, "Counters intersect error\n");
+        assertp(ctrs, "Counters intersect error\n");
+
+        two_counters_t counters = { result, ctrs };
+        counters_iterate(result, &counters, counter_intersect_helper);
+}
+
+/* counter_intersect_helper() assists counters_intersect()
+ */
+static void counter_intersect_helper(void *arg, int key, int count)
+{
+        two_counters_t* counters = arg;
+
+        int num = 0;
+        if (count > counters_get(counters->ctrs, key)) {
+                num = counters_get(counters->ctrs, key);
+        } else {
+                num = count;
+        }
+
+        counters_set(counters->result, key, num);
 }
 
 /*****************************************************
@@ -304,13 +377,36 @@ int test_check_query()
 	print_query(words3, count3);
 	EXPECT(!check_query(words3, count3));
 	count_free(words3);
-
 	EXPECT(count_net() == 0);
 
 	END_TEST_CASE;
 	return TEST_RESULT;
 }
 
+////////////////////////////////////////
+// tests get_scores()
+
+int test_get_scores()
+{
+	START_TEST_CASE("get_scores()");
+
+	char line0[] = "playground and home";
+	char** words0 = count_malloc(3 * sizeof(char*));
+	int count0 = fetch_words(line0, words0);
+	print_query(words0, count0);
+	EXPECT(check_query(words0, count0));
+	
+	index_t *ht = index_load("./index3");
+	index_t* scores = get_scores(ht, words0, count0);
+	index_save(scores, "test_result");
+
+	count_free(words0);
+	count_free(ht);
+	count_free(scores);
+
+	END_TEST_CASE;
+	return TEST_RESULT;
+}
 
 ////////////////////////////////////////
 // main()
@@ -321,6 +417,7 @@ int main(const int argc, const char *argv[])
 
 	failed += test_fetch_words();
 	failed += test_check_query();
+	failed += test_get_scores();
 
 	if (failed) {
 		printf("FAIL %d test cases\n", failed);
